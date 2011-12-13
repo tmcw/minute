152a4895092f8d156759a622fdd684d35bcb70be
Tom MacWright <tom@macwright.org>
2011-12-12 22:09:19 -0500

    mysteriouser, weirder

--- js/site.js
+++ js/site.js
@@ -1,21 +1,24 @@
-var w = 300,
-    h = 500;
+var w = 720,
+    h = 600;
 
 var chart = d3.select('#keystrokes-canvas').append("svg:svg")
-  .attr('width', 500)
+  .attr('width', 720)
   .attr('class', 'Blues')
-  .attr('height', 700);
+  .attr('height', 600);
 
-var day_format = d3.time.format('%a');
+// tick height
+var TH = 80;
+
+var day_format = d3.time.format('%A');
 var full_format = d3.time.format('%I:%M %p %m/%d/%y');
 
 function mtotxt(m) {
     var hr = parseInt(d3.time.format('%I')(m), 10);
     var p = d3.time.format('%p')(m).toLowerCase();
-    return hr + p;
+    return hr == 12 ? hr + p : hr;
 }
 
-$(function() {
+function load() {
   d3.csv("keystrokes.log", function(csv) {
     csv = csv.map(function(c) {
       return {
@@ -44,17 +47,19 @@ $(function() {
     daylabels
       .append('svg:text')
       .attr('class', 'day-label')
-      .attr('x', function(d, i) { return 28 + (i * 80); })
-      .attr('y', function(d, i) { return 40; })
+      .attr('y', function(d, i) { return 20; })
+      .attr('x', function(d, i) { return (-TH * i) - 50; })
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
       .text(function(d) { return day_format(d); });
 
    daylabels
       .append('svg:rect')
       .attr('class', 'day-line')
-      .attr('y', function(d, i) { return 60; })
-      .attr('x', function(d, i) { return 14 + (i * 80); })
-      .attr('height', h)
-      .attr('width', 1);
+      .attr('x', function(d, i) { return 0; })
+      .attr('y', function(d, i) { return 14 + (i * TH); })
+      .attr('height', 1)
+      .attr('width', w);
 
     var hours = dscale.ticks(d3.time.days, 1).map(function(h) {
         var s = d3.time.scale().domain([
@@ -69,12 +74,28 @@ $(function() {
         .enter()
         .append('svg:rect')
         .attr('class', 'hour-label')
-        .attr('height', 1)
-        .attr('width', w)
-        .attr('y', function(d, i) {
-            return ~~((d3.time.hour(d).getHours() / 24) * h) + 60; })
+        .attr('height', w)
+        .attr('width', 1)
         .attr('x', function(d, i) {
+            return ~~((d3.time.hour(d).getHours() / 24) * w); })
+        .attr('y', function(d, i) {
               return 15;
+        });
+
+    chart.selectAll('text.hour-label')
+        .data(_.flatten(hours))
+        .enter()
+        .append('svg:text')
+        .attr('class', 'hour-label')
+        .attr('height', w)
+        .attr('width', 1)
+        .text(function(d) {
+            return mtotxt(d);
+        })
+        .attr('x', function(d, i) {
+            return ~~((d3.time.hour(d).getHours() / 24) * w); })
+        .attr('y', function(d, i) {
+              return 10;
         });
 
     var color = d3.scale.quantize()
@@ -90,40 +111,75 @@ $(function() {
     .enter().append('svg:rect')
       .attr('class', 'strokes')
       .attr('class', function(d) { return 'day q' + color(d.strokes) + '-9'; })
-      .attr('y', function(d, i) {
+      .attr('x', function(d, i) {
         var s = d3.time.scale().domain([
           d3.time.day(d.d),
           d3.time.day(new Date(+d.d + 24*60*60*1000))
         ]);
-        return (s(d.d) * h) + 60;
+        return (s(d.d) * w);
       })
-      .attr('x', function(d) {
-          return (wkscale(d3.time.day(d.d)) * 80) + 15;
+      .attr('y', function(d) {
+          return (wkscale(d3.time.day(d.d)) * TH) + 15;
       })
-      .attr('width', 70)
-      .attr('height', 1)
+      .attr('width', 1)
+      .attr('height', TH)
       .on('mouseover', function(d) {
-        var x = (wkscale(d3.time.day(d.d)) * 80) + 90;
-        d3.select(this).attr('width', 90);
-        d3.select(this).attr('height', 5);
-        chart.append('svg:rect')
-          .attr('class', 'hover-number q' + color(d.strokes) + '-9')
+        var h = d3.select(document.body).append('div')
+          .attr('class', 'hover-number')
           .attr('width', 140)
           .attr('height', 20)
-          .attr('x', x)
-          .attr('y', function() { return d3.event.y - 9; });
-        chart.append('svg:text')
-          .attr('class', 'hover')
-          .text(d.strokes + ' ' + full_format(d.d))
-          .attr('x', x + 5)
-          .attr('y', function() { return d3.event.y + 5; });
+          .style('top', function() { return (d3.event.y + 9) + 'px'; })
+          .style('left', function() { return (d3.event.x - 9) + 'px'; });
+       h.append('span')
+          .attr('class', 'st')
+          .text(d.strokes);
+       h.append('span')
+          .text(full_format(d.d));
       })
       .on('mouseout', function(d) {
-        d3.select(this).attr('width', 70);
-        d3.select(this).attr('height', 1);
-        chart.selectAll('text.hover').remove();
-        chart.selectAll('rect.hover-number').remove();
+        d3.select(document.body).selectAll('div.hover-number').remove();
       });
+
+      var hrly = d3.values(d3.nest()
+        .key(function(d) {
+            return d3.time.hour(d.d);
+        })
+        .rollup(function(ds) {
+            return ds.reduce(function(memo, d) {
+                memo.strokes += d.strokes;
+                memo.d = d3.time.hour(d.d);
+                return memo;
+            }, { strokes: 0 });
+        })
+        .map(csv));
+
+      var hrlycolor = d3.scale.quantize()
+      .domain([d3.min(hrly, function(d) {
+        return d.strokes;
+      }), d3.max(hrly, function(d) {
+        return d.strokes;
+      })])
+     .range(d3.range(2, 9));
+
+
+
+      chart.selectAll('rect.sumstrokes')
+        .data(hrly)
+      .enter().append('svg:rect')
+        .attr('class', 'strokes')
+        .attr('class', function(d) { return 'day q' + hrlycolor(d.strokes) + '-9'; })
+        .attr('x', function(d, i) {
+          var s = d3.scale.linear().domain([
+            d3.time.day(d.d),
+            d3.time.day(new Date(+d.d + 24*60*60*1000))
+          ]);
+          return (s(d.d) * w);
+        })
+        .attr('y', function(d) {
+            return (wkscale(d3.time.day(d.d)) * TH) + 15;
+        })
+        .attr('width', w / 24)
+        .attr('height', 10)
 
       $.ajax('https://api.github.com/users/tmcw/events?callback=test', {
           dataType: 'jsonp',
@@ -138,22 +194,22 @@ $(function() {
               .data(dt)
             .enter().append('svg:rect')
               .attr('class', 'commit')
-              .attr('y', function(d, i) {
+              .attr('x', function(d, i) {
                 var s = d3.time.scale().domain([
                   d3.time.day(d.d),
                   d3.time.day(new Date(+d.d + 24*60*60*1000))
                 ]);
-                return ~~(s(d.d) * h) + 80;
+                return ~~(s(d.d) * w) + 15;
               })
-              .attr('x', function(d) {
-                  return (wkscale(d3.time.day(d.d)) * 80) + 10;
+              .attr('y', function(d) {
+                  return (wkscale(d3.time.day(d.d)) * TH) + 15;
               })
-              .attr('width', 4)
-              .attr('height', 2)
+              .attr('width', 1)
+              .attr('height', TH)
               .on('click', function(d) {
                   window.location = d.repo.url;
               });
           }
       });
   });
-});
+}
