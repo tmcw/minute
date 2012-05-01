5fdb7c2b3f468b11ca173d25a06fdfcafaf186d9
Tom MacWright <tom@macwright.org>
2012-05-01 12:09:34 -0400

    Improve, document

--- README.md
+++ README.md
@@ -7,5 +7,25 @@ Cocoa keycounter application to show off keystrokes. It's a work in progress.
 Uses lots of [d3](http://mbostock.github.com/d3/) behind the scenes,
 with just a quick hint of jQuery to tie things together.
 
-Made to work without real configuration or setup. I symlink `~/log/` to
-`~/Sites/log/`.
+## Installation
+
+[Install minute-agent](https://github.com/tmcw/minute-agent).
+
+Download or git clone this repository into a web-accessible directory.
+On a Mac, the easiest one to use is `~/Sites`, and you'll need to
+enable 'Web Sharing' in `System Preferences -> Sharing` to make
+that work.
+
+Now you'll need to know your username/short username in OSX.
+If you don't know this, open a Terminal window, and type
+`whoami`.
+
+Go to `http://localhost/~yourusername/minute/` and you should
+get a blank page with an error message.
+
+To visualize a snapshot of your keystrokes, _copy_ `keystrokes.log`
+from `~/Documents/minute/keystrokes.log` to `~/Sites/minute/keystrokes.log`.
+
+To have continuously updating version, link the two:
+
+    ln -s ~/Documents/minute/keystrokes.log ~/Sites/minute/keystrokes.log
--- js/site.js
+++ js/site.js
@@ -1,35 +1,6 @@
 var idx = {};
 var d3_behavior_zoomDiv;
 var subdraw;
-// detect the pixels that would be scrolled by this wheel event
-function d3_behavior_zoomDelta() {
-
-  // mousewheel events are totally broken!
-  // https://bugs.webkit.org/show_bug.cgi?id=40441
-  // not only that, but Chrome and Safari differ in re. to acceleration!
-  if (!d3_behavior_zoomDiv) {
-    d3_behavior_zoomDiv = d3.select("body").append("div")
-        .style("visibility", "hidden")
-        .style("top", 0)
-        .style("height", 0)
-        .style("width", 0)
-        .style("overflow-y", "scroll")
-      .append("div")
-        .style("height", "2000px")
-      .node().parentNode;
-  }
-
-  var e = d3.event, delta;
-  try {
-    d3_behavior_zoomDiv.scrollTop = 1000;
-    d3_behavior_zoomDiv.dispatchEvent(e);
-    delta = 1000 - d3_behavior_zoomDiv.scrollTop;
-  } catch (error) {
-    delta = e.wheelDelta || (-e.detail * 5);
-  }
-
-  return delta * 0.005;
-}
 
 function draw() {
   var w = window.innerWidth - 10,
@@ -37,45 +8,11 @@ function draw() {
       top = 0,
       dragStart = false;
 
-  function mousewheel() {
-      h += d3_behavior_zoomDelta() * 50;
-      top += d3_behavior_zoomDelta() * 25;
-      if (h < (window.innerHeight - 10)) {
-        h = window.innerHeight - 10;
-      }
-      if (top < 0) top = 0;
-      if (top > (h - window.innerHeight)) top = h - window.innerHeight;
-      subdraw();
-  }
-
-  function mousedown() {
-      dragStart = d3.event.screenY;
-  }
-
-  function mousemove() {
-      if (dragStart === false) return;
-      top += dragStart - d3.event.screenY;
-      dragStart = d3.event.screenY;
-      if (top < 0) top = 0;
-      if (top > (h - window.innerHeight)) top = h - window.innerHeight;
-      subdraw();
-  }
-
-  function mouseup(e) {
-      if (top < 0) top = 0;
-      dragStart = false;
-  }
-
   var chart = d3.select('#keystrokes-canvas').append("svg:svg")
     .attr('id', 'chart')
     .attr('width', w)
     .attr('class', 'Greys')
-    .attr('height', h)
-    .on('mousedown.drag', mousedown)
-    .on('mousemove.drag', mousemove)
-    .on('mouseup.drag', mouseup)
-    .on('mousewheel.zoom', mousewheel)
-    .on('DOMMouseScroll.zoom', mousewheel);
+    .attr('height', h);
 
   // tick height
   var day_format = d3.time.format('%A');
@@ -83,6 +20,11 @@ function draw() {
   var biminutes = 1440 / 2;
 
   d3.csv("keystrokes.log", function(csv) {
+    if (!csv) {
+        d3.select('#help')
+            .style('display', 'block');
+        return;
+    }
     var total_keystrokes = 0;
     csv = csv.map(function(c) {
       var dia = d3.time.day(new Date(c.minute * 1000));
@@ -137,12 +79,14 @@ function draw() {
      .range(d3.range(2, 9));
 
    var strokes = d3.scale.sqrt()
-      .exponent(0.1)
+      .exponent(0.3)
       .domain([d3.min(csv, function(d) {
         return d.strokes;
       }), d3.max(csv, function(d) {
         return d.strokes;
       })]);
+
+    d3.select('#loader').remove();
 
     var dayrect = chart.selectAll('rect.day')
         .data(csv)
@@ -160,131 +104,15 @@ function draw() {
           ]).range([0, h])(d.d);
         })
         .attr('width', function(d) {
-          return strokes(d.strokes) * ~~(w/(n_days));
+          return ~~(w/n_days) - 2;
         })
-        .attr('height', 1);
-
-    var hrline = chart.selectAll('rect.hour-line')
-        .data(d3.range(0, 24))
-        .enter().append('svg:rect')
-        .attr('class', function(d) { return 'hour-line'; })
-        .attr('x', function(d, i) {
-          return 0;
-        })
+        .attr('height', 1)
         .attr('y', function(d) {
-          return ~~((d / 24) * h);
-        })
-        .attr('width', w)
-        .attr('height', 1);
-
-    subdraw = function() {
-      hrline.attr('y', function(d) {
-          return ~~((d / 24) * h) - top;
+          return d3.time.scale().domain([
+            d3.time.day(d.d),
+            d3.time.day(new Date(+d.d + 24*60*60*1000))
+          ]).range([0, h])(d.d) - top;
         });
-
-      dayrect.attr('y', function(d) {
-        return d3.time.scale().domain([
-          d3.time.day(d.d),
-          d3.time.day(new Date(+d.d + 24*60*60*1000))
-        ]).range([0, h])(d.d) - top;
-      });
-    };
-
-    subdraw();
-
-      function transitionStack() {
-        chart.selectAll('rect.day')
-          .transition()
-            .duration(10)
-            .delay(function(d, i) { return (i) * 10; })
-            .attr("height", 1)
-            .attr('width', w/n_days)
-            .attr('x', function(d, i) {
-                return ~~day(d.day);
-            })
-            .attr("y", function(d, i) {
-              var s = d3.time.scale().domain([
-                d3.time.day(d.d),
-                d3.time.day(new Date(+d.d + 24*60*60*1000))
-              ]);
-              var preh = s(d.d);
-              return h - (d.idx / biminutes * h);
-            });
-      }
-
-      function transitionNormal() {
-        chart.selectAll('rect.day')
-        .transition()
-          .duration(100)
-          .delay(function(d, i) { return (i) * 10; })
-          .attr("height", 1)
-          .attr('width', w/n_days)
-          .attr('x', function(d, i) {
-              return ~~day(d.day);
-          })
-          .attr("y", function(d, i) {
-            var s = d3.time.scale().domain([
-              d3.time.day(d.d),
-              d3.time.day(new Date(+d.d + 24*60*60*1000))
-            ]);
-            return (s(d.d) * h);
-          });
-      }
-
-      function transitionTime() {
-        chart.selectAll('rect.day')
-        .transition()
-          .duration(50)
-          .delay(function(d, i) { return (i) * 10; })
-          .attr('width', 1)
-          .attr("x", function(d, i) {
-            return dscale(d.d) * w;
-          })
-          .each('end', function() {
-            d3.select(this)
-              .transition()
-              .delay(function(d, i) { return (i) * 10; })
-              .duration(500)
-              .attr("y", 0)
-              .attr("height", h);
-           });
-      }
-
-      var playerline = chart.append('svg:rect')
-        .attr('class', 'play')
-        .attr('x', 0)
-        .attr('height', 2)
-        .attr('width', w)
-        .attr('y', 100);
-
-     var playertext = chart.append('svg:text')
-        .attr('class', 'playtext')
-        .attr('x', 0)
-        .attr('y', 100);
-
-      chart.on('mousemove', function() {
-         var mousey = d3.svg.mouse(this)[1];
-         playerline
-            .attr('y', mousey);
-
-         playertext
-            .attr('y', mousey - 10)
-            .text(function() {
-                return ''; // d3.time.format('%B %e')();
-            });
-      });
-
-      d3.select('#stack').on('click', function() {
-        transitionStack();
-      });
-
-      d3.select('#timeflux').on('click', function() {
-        transitionTime();
-      });
-
-      d3.select('#normal').on('click', function() {
-        transitionNormal();
-      });
 
       d3.select('#total_keystrokes')
         .text(function() {
@@ -295,8 +123,7 @@ function draw() {
           };
           for (var i in prec) {
             if (total_keystrokes > prec[i]) {
-              return Math.round(total_keystrokes / prec[i]) +
-                ' ' + i;
+              return Math.round(total_keystrokes / prec[i]) + ' ' + i;
             }
           }
         });
