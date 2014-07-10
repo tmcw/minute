b76e8bdadf9bda4bc81fe4f94c012286fb3a5b54
Matthew Chase Whittemore <matthew@socialtables.com>
2014-07-10 00:04:36 -0400

    bring in better canvas code for basic-canvas

--- js/basic-canvas.js
+++ js/basic-canvas.js
@@ -1,108 +1,90 @@
 var idx = {};
 
 function draw() {
-  var w = window.innerWidth - 10,
-  h = window.innerHeight - 10,
-  top = 0,
-  dragStart = false;
+  var width = window.innerWidth - 10,
+  height = window.innerHeight - 10;
 
   var chart = d3.select('#keystrokes-canvas').append('canvas')
       .attr('id', 'chart')
-      .attr('width', w)
+      .attr('width', width)
       .attr('class', 'Greys')
-      .attr('height', h).node();
+      .attr('height', height).node();
   var ctx = chart.getContext('2d');
-
-  // tick height
-  var day_format = d3.time.format('%A');
-  var full_format = d3.time.format('%I:%M %p %m/%d/%y');
-  var biminutes = 1440 / 2;
+  
 
   d3.csv('keystrokes.log', function(csv) {
-    if (!csv) {
-      d3.select('#help')
-      .style('display', 'block');
-      return;
+    
+    //hack to get the data into the state the copied code expects
+    var lines = [];
+    for(var i=0; i<csv.length; i++){
+      lines.push(csv[i].minute+","+csv[i].strokes);
     }
-    var total_keystrokes = 0;
-    csv = csv.map(function(c) {
-      var d = new Date(c.minute * 1000);
-      var day = d3.time.day(d);
-      total_keystrokes += parseInt(c.strokes, 10);
-      return {
-        d: d,
-        day: day,
-        strokes: parseInt(c.strokes, 10)
-      };
-    });
 
-    var dscale = d3.time.scale().domain([
-        d3.min(csv, function(d) { return d.d; }),
-        d3.max(csv, function(d) { return d.d; })
-    ]);
+    //from here down is all from https://gist.github.com/tmcw/3955198
 
-    var a = d3.min(csv, function(d) {
-      return d.day;
-    }),
-    b = d3.max(csv, function(d) {
-      return d.day;
-    });
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, width, height);
 
-    a = d3.time.day(a);
-    b = new Date(d3.time.day(b).getTime() + 24*60*60*1000);
+    var gaps = {};
 
-    var n_days = d3.time.days(a, b).length;
+    var days = [];
 
-    var day = d3.time.scale()
-      .domain([a, b])
-      .range([0, w]);
+    function secs(d) {
+        return (d.getHours() * 60 * 60) +
+            (d.getMinutes() * 60) +
+            (d.getSeconds());
+    }
 
-    var hours = dscale.ticks(d3.time.days, 1).map(function(h) {
-      var s = d3.time.scale().domain([
-          d3.time.day(h),
-          d3.time.day(new Date(+h + 24 * 60 * 60 * 1000))
-      ]);
-      return s.ticks(d3.time.hours, 2);
-    });
+    function stamp(d) {
+        return d.getDate() + ',' + d.getMonth() + '-' + d.getYear();
+    }
 
-    var color = d3.scale.quantize()
-    .domain([d3.min(csv, function(d) {
-      return d.strokes;
-    }), d3.max(csv, function(d) {
-      return d.strokes;
-    })])
-    .range(d3.range(2, 9));
+    var days = {};
+
+    for (var i = 0; i < lines.length; i++) {
+        var t = new Date(+lines[i].split(',')[0] * 1000);
+        var v = +lines[i].split(',')[1];
+        var s = stamp(t);
+        if (!days[s]) days[s] = [];
+        days[s].push([secs(t), v]);
+    }
+
+    var full = (60 * 60 * 24);
+
+    function sy(x) {
+        return Math.floor((x / full) * height);
+    }
+    var ndays = Object.keys(days).length;
+
+    function sx(x) {
+        return (x / ndays) * width;
+    }
+
+    var numDays = Object.keys(days).length;
+    var wid = Math.floor(width/numDays);
+
+    var scale = ['#111',
+        '#223',
+        '#336',
+        '#45a',
+        '#76e',
+        '#99f',
+        '#fcf',
+        '#faf'];
+
+    var dayn = 0;
+    for (var d in days) {
+        var day = days[d];
+        for (var t = 0; t < day.length; t++) {
+            var v = day[t][1];
+            var sv = Math.max(0, Math.min(Math.sqrt(v) /5, 6));
+            ctx.fillStyle = scale[sv];
+            ctx.fillRect(~~sx(dayn), ~~sy(day[t][0]), wid, 1);
+        }
+        dayn++;
+    }
 
     d3.select('#load').remove();
 
-    var dayscale = d3.time.scale().range([0, h - top]);
-
-    ctx.fillStyle = '#fff';
-
-    for (var i = 0; i < csv.length; i++) {
-        var d = csv[i];
-        ctx.fillRect(
-          ~~day(d.day),
-          dayscale.domain([
-              d.day,
-              d3.time.day.offset(new Date(d.day.getTime()), 1)
-          ])(d.d),
-          ~~(w/n_days) - 2,
-          1);
-    }
-
-    d3.select('#total_keystrokes')
-        .text(function() {
-          var prec = {
-            'million': 1000000,
-            'thousand': 1000,
-            'hundred': 100
-          };
-          for (var i in prec) {
-            if (total_keystrokes > prec[i]) {
-              return Math.round(total_keystrokes / prec[i]) + ' ' + i;
-            }
-          }
-        });
   });
 }
